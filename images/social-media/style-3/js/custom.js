8479eda921f5d14ffc622b70ea4e09411c05fd9f
ekstevens <ekstevens@outlook.com>
2014-05-23 08:36:52 -0700

    052314-836AM

--- images/social-media/style-3/js/custom.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-$('.menu-item').hover(function () {
-    $('#active', this).toggleClass('active');
-    $('#active', this).css({'display':'block'}); //adds context
-});
-});
