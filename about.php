bca18e7e757eefb93aeb405739a49e5d1773d927
ekstevens <ekstevens@outlook.com>
2014-05-27 09:05:59 -0700

    about. still needs formatting and whatnot.

--- about.php
+++ about.php
@@ -78,49 +78,172 @@
 <!-- Content  -->
           <div id="content-container">
                <div class="section-container" id="first-section-margin">
-                    <div class="about-video-overlay">
-                         Introducing the<br />
-                         <img src="img/logos/ecoqube-logo.png" /><br />
-                         a beautiful, compact, all-in-one<br />
-                         aquatic ecosystem for your home or office.
-                    </div><!-- .about-video-overlay -->
-                    <div class="about-play-icon">
-                         &#xe817;
-                    </div><!-- .about-play-icon -->
-                    <div class="about-video-image">
-                         <img src="img/ecoqube1.jpg" />
-                    </div><!-- .about-video-image -->
+                    <div class="about-founders-image">
+                         <img src="img/about/team-1.jpg" />
+                    </div><!-- .about-founders-image -->
                </div><!-- .section-container #first-section-margin -->
 
-               <div class="section-container">
-                    <div class="section-content">
-                         <div class="section-heading">
-                              <a id="buyittoday" class="anchor"></a>Purchase an EcoQube Today
-                         </div><!-- .section-heading -->
-                         <br />
-                         The aquarium kit provides you with the easiest and most sustainable way to keep fish and grow plants!
-                         <br />
-                         The complete EcoQube includes the following:
-                         <br />
-                         LED Light
-                         <br />
-                         LED UV Sterilizer
-                         <br />
-                         Aquaponics filter
-                         <br />
-                         Aquarium
-                    </div><!-- section-content -->
-               </div><!-- .section-container -->
+               <!-- Our Story -->
+                    <div class="section-container">
+                         <div class="section-content">
+                              <div class="section-heading">
+                                   <a id="ourstory" class="anchor"></a>Our names are Kevin and Eric.
+                              </div><!-- .section-heading -->
+                              <br />
+                              We are UC San Diego students and aquarium geeks who met in high school and instantly became intrigued by planted aquatic ecosystems.
+                              <br />
+                              As we got older, we realized the bigger challenges humanity as a whole faces in terms of food and water shortages, so we decided to use our knowledge of aquariums to deliver the concept of aquaponics to as many people as possible.
+                              <br /><br />
+                              <div id="about-ourstory-timeline">
+                                   <span class="about-ourstory-heading">The Beginning</span>
+                                   <br />
+                                   We started out working as operations managers, biology assistants, sales representatives, and installing aquariums at various locations.
+                                   <br /><br />
+                                   <div id="about-ourstory-thebeginning"><img src="img/about/aquaticcentral.png" /> <img src="img/about/californiaacademyofsciences.png" /> <img src="img/about/aquaforestaquarium.png" /></div>
+                                   <br />
+                                   <img src="img/about/ourstory-4.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-5.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-6.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-13.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-7.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-10.jpg" class="about-ourstory-timeline-images" />
+                                   <br />
+                                   With knowledge and experience, we established ADI with the fundamental goals to build a business around a solution for the aquarium industry and to raise environmental awareness.
+                                   <br />
+                                   Eventually, we decided to start our own online aquarium store while selling shrimp out of a garage in San Francisco.
+                                   <br />
+                                   <img src="img/about/ourstory-1.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-9.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-2.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-3.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-18.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-21.jpg" class="about-ourstory-timeline-images" />
+                                   <br />
+                                   We continued pursuing our passion for connecting people with nature through aquatic ecosystem installations.
+                                   <br /><br />
+                                   <span class="about-ourstory-heading">The Evolution</span>
+                                   <br />
+                                   We even brought our love of aquariums with us to college.
+                                   <br />
+                                   <img src="img/about/ourstory-11.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-12.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-8.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-14.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-15.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-17.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-16.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-19.jpg" class="about-ourstory-timeline-images" /> <img src="img/about/ourstory-20.jpg" class="about-ourstory-timeline-images" />
+                                   <br /><br />
+                                   <span class="about-ourstory-heading">The Company Today</span>
+                                   <br />
+                                   But now, we'd like to expand our reach to you through the EcoQube, a miniature aquatic ecosystem that we created to deliver the practical solution and beauty of aquaponics to households and offices around the world!
+                                   <br />
+                                   <img src="img/ecoqube2.jpg" class="about-ourstory-timeline-images" />
+                                   <br />
+                                   We know that we can’t solve the many problems in our world with a fish tank, no matter how beautiful it is, but we hope to connect people to our environment and influence the way they perceive the larger issues in our world today.
+                                   <br /><br />
+                              </div><!-- about-ourstory-timeline -->
+                              “Because the crazy ones who think they can change the world are the ones who do.” –Apple
+                         </div><!-- section-content -->
+                    </div><!-- .section-container -->
 
-               <div class="section-container" id="last-section-margin">
-                    <div class="section-content">
-                         <div class="section-heading">
-                              <a id="press" class="anchor"></a>Press Releases
-                         </div><!-- .section-heading -->
-                         <br />
-                         <img src="img/ecoqube/press.png" />
-                    </div><!-- section-content -->
-               </div><!-- .section-container #last-section-margin -->
+               <!-- Values -->
+                    <div class="section-container">
+                         <div class="section-content">
+                              <div class="section-heading">
+                                   <a id="values" class="anchor"></a>Vision and Values
+                              </div><!-- .section-heading -->
+                              <br />
+                              Innovate with Nature.
+                              <br />
+                              We have a vision to be much more than an online vendor for aquarium goods and an installation company.
+                              <br /><br />
+                              Revolutionize Aquarium Trade – by completely eliminating intermediate suppliers, distributors and aquarium retail shops. The livestock we sell comes directly from over 10 international farms. We will later advance into breeding our very own livestock and manufacturing our own line of products. The consolidation will benefit you, the environment, the economy and the aquarium industry in the following ways:
+                              <br /><br />
+                              Less Transport- Less time and resources spent on transporting livestock directly leads to unsurpassed quality and…
+                              <br /><br />
+                              Less Environmental Impact- Less fish are lost and less fossil fuel is burned. Resulting in more:
+                              <br /><br />
+                              Competitive Pricing- Savings are then passed onto you and higher profit margins allows for…
+                              <br /><br />
+                              Domestic Jobs- Happy local employees will then provide you with unmatchable…
+                              <br /><br />
+                              Quality and Service- Something all of our customers deserve!
+                              <br /><br />
+                              Innovate with Nature- Through using ADI’s compilation of innovative methods of sustainable Aquatic Ecosystem installations, ADI is determined to transform the way people typically view aquariums as an unnecessary fish tank to an essential component of every home, office, garden or roof. Utilizing the same methods, equipment and technology, ADI hopes to incorporate such technologies into food production, home gardening, landscaping and sustainable architecture.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                              <br /><br />
+                              90%+ of the aquarium industry is salt water focused, where most livestock are brutally harvested from the wild. Freshwater isn’t much better. An estimated 30% of ALL freshwater aquarium livestock is lost due to deaths BEFORE they get to the end customer’s aquariums. With years of experience, ADI is well equipped with practicing the best methods and direct FARMED sources limiting deaths to less than 5% from farms all the way to our customer’s aquariums. This dramatically increases quality of livestock, reduces aquarists’ carbon footprint on the environment and minimizes cost of livestock, creating a triple win situation. Hence our motto, “Revolutionize Aquarium Trade.”
+                              <br /><br />
+                              Planted Aquatic Ecosystems are indisputably beautiful. ADI is set on challenging the status quo of the goldfish in a bowl for fresh water aquariums and the idea of advanced aquarium keeping being only limited to salt water aquariums. ADI believes that the artistic and aesthetic elements of an innovative 90% self-sustaining Aquatic Ecosystem installation will attract a large audience and has the power to convey the significance of a balanced, natural ecosystem. Hence our other motto, “Innovate with Nature.”  We have already started working with various environmental awareness groups at the University of California San Diego to begin this green campaign.
+                              <br /><br />
+                              When dealing with ADI, you are more than just a customer; you become part of the revolution.
+                         </div><!-- section-content -->
+                    </div><!-- .section-container -->
+
+               <!-- The Team -->
+                    <div class="section-container">
+                         <div class="section-content">
+                              <div class="section-heading">
+                                   <a id="theteam" class="anchor"></a>The Team
+                              </div><!-- .section-heading -->
+                              <br />
+                              <img src="img/about/team-2.jpg" class="about-team-members" />
+                         </div><!-- section-content -->
+                    </div><!-- .section-container -->
+
+               <!-- Affiliates -->
+                    <div class="section-container" id="last-section-margin">
+                         <div class="section-content">
+                              <div class="section-heading">
+                                   <a id="affiliates" class="anchor"></a>Affiliates, Sponsorships, and Philanthropy
+                              </div><!-- .section-heading -->
+                              <br />
+                              <span class="about-affiliates-names">Chris E. Yin</span>
+                              <br />
+                              Chief Operating Officer at Bespoke Partners and founder of Pathways Ventures, an on-campus incubator at University of California, San Diego.
+                              <br /><br />
+                              <span class="about-affiliates-names">Marshall Kauf</span>
+                              <br />
+                              Senior Associate at Bespoke Partners as well as member of the Entrepreneur’s Organization in Los Angeles.
+                              <br /><br />
+                              <span class="about-affiliates-names">Chris Sims</span>
+                              <br />
+                              Owner of <a href="http://www.aquaticcentral.com">Aquatic Central</a>, one of the largest aquarium stores in Northern California.
+                              <br /><br />
+                              <span class="about-affiliates-names">San Francisco Bay Area Aquatic Plant Society</span>
+                              <br />
+                              A bay area aquarium community with over 1000 members. The most informative forum for freshwater aquariums in Northern California. <a href="http://www.sfbaaps.org">SFBAAPS</a>
+                              <br /><br />
+                              <span class="about-affiliates-names">Curtis Chinn</span>
+                              <br />
+                              Biology teacher at Galileo Academy of Sciences and Technology. Graduated with degree in biological sciences at University of California, Davis. Curtis was in charge of Galileo’s Aqua Culture Club and sought to introduce and teach students about aquatic ecosystems and the science behind them.
+                              <br /><br />
+                              <span class="about-affiliates-names">Patrick Carter</span>
+                              <br />
+                              Biologist at the California Academy of Sciences in San Francisco and former employee at Aqua Forest Aquarium. He’s also the founder of <a href="http://http://www.urban-aquatics.com/">Urban Aquatics</a>- an installation, maintenance, and consultation company for aquatic related hobbies.
+                              <br /><br />
+                              <span class="about-affiliates-names">Nick Xyong</span>
+                              <br />
+                              Active aquarium hobbyist with years of experience in aquatic plants and livestock. Founder of <a href="http://www.speedieaquatics.com">Speedie Aquatics</a>- currently one of the largest online retailers of plants and invertebrates.
+                              <br /><br />
+                              <span class="about-affiliates-names">David Kim</span>
+                              <br />
+                              BS in Biochem-Cellular Biology, BA in Art at University of California, San Diego. David aspires to use biological sciences to create sustainable ecosystems while presenting it as an art. At the University of California, San Diego, David installed <a href="http://vimeo.com/38627965">a unique aquatic ecosystem</a> for their Sixth College administrative offices.
+                              <br /><br />
+                              <span class="about-affiliates-names"><a href="https://www.abasd.org/home.php">Asian Business Association of San Diego</a></span>
+                              <br />
+                              A non-profit 501(c)(6) organization that provides a strong voice on business, cultural and political issues of interest to San Diego’s Asian and Pacific Islander community.
+                              <br /><br />
+                              <span class="about-affiliates-names"><a href="http://galileoweb.org/chinnc/old-pages/aquarium-club/">Galileo Academy Aquarium Club</a></span>
+                              <br />
+                              A student initiated community, sponsored by the Futurama After School program.
+                              <br /><br />
+                              <span class="about-affiliates-names"><a href="http://scapeclub.org/">SCAPE - Southern California Aquatic Plant Enthusiasts</a></span>
+                              <br />
+                              A local, non profit group formed of the most dedicated Aquatic Plant Enthusiasts in Southern California.
+                              <br /><br />
+                              <span class="about-affiliates-names"><a href="http://www.plantedtank.net/">The Planted Tank</a></span>
+                              <br />
+                              A community dedicated to aquarium afficianados.
+                         </div><!-- section-content -->
+                    </div><!-- .section-container #last-section-margin -->
 
           </div><!-- #content-container -->
 
