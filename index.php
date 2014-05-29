1d637432d476aee8c7b242f5ab8576c062f53982
Tiffany Tse <titse@ucsd.edu>
2014-05-29 00:28:13 -0700

    no changes

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-          <title>Home</title>
+     <head>
+          <title>Aqua Design Innovations</title>
           <link type="text/css" rel="stylesheet" href="css/global.css"/>
           <link type="text/css" rel="stylesheet" href="css/index.css"/>
-          <link rel="shortcut icon" href="img/logos/favicon.ico">
-
+		
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1">
+         
 		 <!--jquery library-->
 		 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 			<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 		<!--nerve slider--->
 			<script src="http://code.ryanbruzan.net/nerveSlider/jquery.nerveSlider.min.js"></script>
 			<link rel="stylesheet" href="http://code.ryanbruzan.net/nerveSlider/nerveSlider.min.css" />
-		
-	<!--activate the nerve slider-->	
-<script type="text/javascript">
+
+
+
+<script>
 $(document).ready(function(){
 	$(".slider").nerveSlider({
 		sliderWidth: "100%",
@@ -27,10 +28,12 @@ $(document).ready(function(){
 		});
 });
 </script>
-</head>
-<body>
 
-     <!-- Header -->
+			
+     </head>
+     <body>
+
+<!-- Header -->
           <div id="header-container">
                <header>
                     <div id="header-logo">
@@ -48,17 +51,17 @@ $(document).ready(function(){
                                         <li><a href="ecoqube.php#press">Press Releases</a></li>
                                    </ul>
                               </li>
-                              <li><a href="shop/index.php" class="header-heading">Shop</a>
+                              <li><a href="shop/index.php" class="header-heading">Shop</a> 
                                    <ul class="sub_menu">
                                         <li><a href="shop/ecoqube.php">EcoQube</a></li>
                                         <li><a href="shop/livestock.php">Livestock</a></li>
                                         <li><a href="shop/plants.php">Plants</a></li>
                                         <li><a href="shop/accessories.php">Accessories</a></li>
-                                     </ul>
+                                     </ul>                           
                               </li>
                               <li><a href="services.php" class="header-heading">Services</a>
                                    <ul  class="sub_menu">
-                                        <li><a href="services.php#contact">Contact</a></li>
+                                        <li><a href="services.php#contact">Contact</a></li>    
                                         <li><a href="services.php#installations">Installations</a></li>
                                         <li><a href="services.php#maintenance">Maintenance</a></li>
                                         <li><a href="services.php#gallery">Gallery</a></li>
@@ -94,8 +97,8 @@ $(document).ready(function(){
                </header>
           </div><!-- #header-container -->
 
-     <!-- Content  -->
-                        <div id="content-container">
+<!-- Content  -->
+          <div id="content-container">
                <div class="section-container" id="first-section-margin">
 				<div class ="slider">
 				<div class="picture1">
@@ -119,104 +122,104 @@ $(document).ready(function(){
                </div><!-- .section-container #first-section-margin -->
           </div><!-- #content-container -->
 
-     <!-- Footer  -->
-               <div id="footer-container">
-                    <div id="footer-topbar">
-                         <form name="input" id="footer-subscribe-form">
-                              Sign up for weekly deals!
-                              <input type="text" placeholder="Example@Email.com" />
-                              <input type="submit" value="Subscribe"/>
-                         </form><!-- #footer-subscribe-form [this id tag is not yet in stylesheet.css]-->
-                    </div><!-- #footer-topbar -->
+<!-- Footer  -->
+          <div id="footer-container">
+               <div id="footer-topbar">
+                    <form name="input" id="footer-subscribe-form">
+                         Sign up for weekly deals!
+                         <input type="text" placeholder="Example@Email.com" />
+                         <input type="submit" value="Subscribe"/>
+                    </form><!-- #footer-subscribe-form [this id tag is not yet in stylesheet.css]-->
+               </div><!-- #footer-topbar -->
 
-                    <footer>
-                         <div id="footer-left-col">
-                              <div id="footer-logo">
-                                   <a href="index.php"><img src="img/logos/footer-logo.png" /></a>
-                              </div><!-- #footer-logo -->
-                         </div><!-- #footer-left-col -->
+               <footer>
+                    <div id="footer-left-col">
+                         <div id="footer-logo">
+                              <a href="index.php"><img src="img/logos/footer-logo.png" /></a>
+                         </div><!-- #footer-logo -->
+                    </div><!-- #footer-left-col -->
 
-                         <div id="footer-right-col">
-                              <div id="footer-contact">
-                                   <div id="footer-phone-email">
-                                        <ul>
-                                             <li>
-                                                  <a href="mailto:info@aquadesignainnovations.com"><span id="footer-icon-email">&#xe801;</span> info@aquadesignainnovations.com</a>
-                                             </li>
-                                             <li>
-                                                  <a href="tel:8582243474"><span id="footer-icon-phone">&#xe800;</span>(888) 224-FISH</a>
-                                             </li>
-                                        </ul>
-                                   </div>
-                                   <div id="footer-social-media">
-                                        <ul>
-                                             <li>
-                                                  <a href="https://www.facebook.com/AQUADESIGNINNOVATIONS"><span id="footer-icon-fb">&#xe81b;</span></a>
-                                             </li>
-                                             <li>
-                                                  <a href="https://twitter.com/DesignADI"><span id="footer-icon-tw">&#xe81a;</span></a>
-                                             </li>
-                                        </ul>
-                                   </div>
-                              </div><!-- #footer-contact -->
+                    <div id="footer-right-col">
+                         <div id="footer-contact">
+                              <div id="footer-phone-email">
+                                   <ul>
+                                        <li>
+                                             <a href="mailto:info@aquadesignainnovations.com"><span id="footer-icon-email">&#xe801;</span> info@aquadesignainnovations.com</a>
+                                        </li>
+                                        <li>
+                                             <a href="tel:8582243474"><span id="footer-icon-phone">&#xe800;</span>(888) 224-FISH</a>
+                                        </li>
+                                   </ul>
+                              </div>
+                              <div id="footer-social-media">
+                                   <ul>
+                                        <li>
+                                             <a href="https://www.facebook.com/AQUADESIGNINNOVATIONS"><span id="footer-icon-fb">&#xe81b;</span></a>
+                                        </li>
+                                        <li>
+                                             <a href="https://twitter.com/DesignADI"><span id="footer-icon-tw">&#xe81a;</span></a>
+                                        </li>
+                                   </ul>
+                              </div>
+                         </div><!-- #footer-contact -->
 
-                              <div id="footer-sitemap-container">
-                                   <div class="footer-heading-1">
-                                   </div><!-- .footer-heading-1 -->
+                         <div id="footer-sitemap-container">
+                              <div class="footer-heading-1">
+                              </div><!-- .footer-heading-1 -->
 
-                                   <div id="footer-sitemap">
-                                        <ul>
-                                             <li><span class="footer-heading-2"><a href="ecoqube.php">EcoQube</a></span></li>
-                                             <li><a href="ecoqube.php#howitworks">How It Works</a></li>
-                                             <li><a href="ecoqube.php#keyfeatures">Key Features</a></li>
-                                             <li><a href="ecoqube.php#buyittoday">Buy It Today</a></li>
-                                             <li><a href="ecoqube.php#fishandplants">Fish & Plants</a></li>
-                                             <li><a href="ecoqube.php#press">Press Releases</a></li>
-                                        </ul>
-                                        <ul>
-                                             <li><span class="footer-heading-2"><a href="shop/index.php">Shop</a></span></li>
-                                             <li><a href="shop/ecoqube.php">EcoQube</a></li>
-                                             <li><a href="shop/livestock.php">Livestock</a></li>
-                                             <li><a href="shop/plants.php">Plants</a></li>
-                                             <li><a href="shop/accessories.php">Accessories</a></li>
-                                        </ul>
-                                        <ul>
-                                             <li><span class="footer-heading-2"><a href="services.php">Services</a></span></li>
-                                             <li><a href="services.php#contact">Contact</a></li>
-                                             <li><a href="services.php#installations">Installations</a></li>
-                                             <li><a href="services.php#maintenance">Maintenance</a></li>
-                                             <li><a href="services.php#gallery">Gallery</a></li>
-                                        </ul>
-                                        <ul>
-                                             <li><span class="footer-heading-2"><a href="about.php">About</a></span></li>
-                                             <li><a href="about.php#ourstory">Our Story</a></li>
-                                             <li><a href="about.php/#values">Values</a></li>
-                                             <li><a href="about.php/#theteam">The Team</a></li>
-                                             <li><a href="about.php/#affiliates">Affiliates</a></li>
-                                        </ul>
-                                        <ul>
-                                             <li><span class="footer-heading-2"><a href="learn/index.php">Learn</a></span></li>
-                                             <li><a href="learn/blog.php">Blog</a></li>
-                                             <li><a href="learn/ecosystems.php">Ecosystems</a></li>
-                                        </ul>
-                                        <ul>
-                                             <li><span class="footer-heading-2"><a href="faq.php">FAQ</a></span></li>
-                                             <li><a href="faq.php#ecoqube">EcoQube</a></li>
-                                             <li><a href="faq.php#shop">Shop</a></li>
-                                             <li><a href="faq.php#services">Services</a></li>
-                                             <li><a href="faq.php#shipping">Shipping</a></li>
-                                             <li><a href="faq.php#warranty">Warranty</a></li>
-                                        </ul>
-                                   </div><!-- #footer-sitemap -->
-                              </div><!-- #footer-sitemap-container -->
-                         </div><!-- #footer-right-col -->
+                              <div id="footer-sitemap">
+                                   <ul>
+                                        <li><span class="footer-heading-2"><a href="ecoqube.php">EcoQube</a></span></li>
+                                        <li><a href="ecoqube.php#howitworks">How It Works</a></li>
+                                        <li><a href="ecoqube.php#keyfeatures">Key Features</a></li>
+                                        <li><a href="ecoqube.php#buyittoday">Buy It Today</a></li>
+                                        <li><a href="ecoqube.php#fishandplants">Fish & Plants</a></li>
+                                        <li><a href="ecoqube.php#press">Press Releases</a></li>
+                                   </ul>
+                                   <ul>
+                                        <li><span class="footer-heading-2"><a href="shop/index.php">Shop</a></span></li>
+                                        <li><a href="shop/ecoqube.php">EcoQube</a></li>
+                                        <li><a href="shop/livestock.php">Livestock</a></li>
+                                        <li><a href="shop/plants.php">Plants</a></li>
+                                        <li><a href="shop/accessories.php">Accessories</a></li>
+                                   </ul>
+                                   <ul>
+                                        <li><span class="footer-heading-2"><a href="services.php">Services</a></span></li>
+                                        <li><a href="services.php#contact">Contact</a></li>    
+                                        <li><a href="services.php#installations">Installations</a></li>
+                                        <li><a href="services.php#maintenance">Maintenance</a></li>
+                                        <li><a href="services.php#gallery">Gallery</a></li>
+                                   </ul>
+                                   <ul>
+                                        <li><span class="footer-heading-2"><a href="about.php">About</a></span></li>
+                                        <li><a href="about.php#ourstory">Our Story</a></li>
+                                        <li><a href="about.php/#values">Values</a></li>
+                                        <li><a href="about.php/#theteam">The Team</a></li>
+                                        <li><a href="about.php/#affiliates">Affiliates</a></li>
+                                   </ul>
+                                   <ul>
+                                        <li><span class="footer-heading-2"><a href="learn/index.php">Learn</a></span></li>
+                                        <li><a href="learn/blog.php">Blog</a></li>
+                                        <li><a href="learn/ecosystems.php">Ecosystems</a></li>
+                                   </ul>
+                                   <ul>
+                                        <li><span class="footer-heading-2"><a href="faq.php">FAQ</a></span></li>
+                                        <li><a href="faq.php#ecoqube">EcoQube</a></li>
+                                        <li><a href="faq.php#shop">Shop</a></li>
+                                        <li><a href="faq.php#services">Services</a></li>
+                                        <li><a href="faq.php#shipping">Shipping</a></li>
+                                        <li><a href="faq.php#warranty">Warranty</a></li>
+                                   </ul>
+                              </div><!-- #footer-sitemap -->
+                         </div><!-- #footer-sitemap-container -->
+                    </div><!-- #footer-right-col -->
 
-                         <div id="footer-copyright">
-                              <p>Aqua Design Innovations 2014 - All Rights Reserved</p>
-                         </div><!-- #footer-copyright -->
+                    <div id="footer-copyright">
+                         <p>Aqua Design Innovations 2014 - All Rights Reserved</p>
+                    </div><!-- #footer-copyright -->
 
-                         </footer>
-               </div><!-- #footer-container -->
+                    </footer>
+          </div><!-- #footer-container -->
 
-</body>
+     </body>
 </html>
